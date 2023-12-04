0ba27fa8f7386ac5463510fa7b37f1d62e15b6a5
Mohammed Abdelhay <mamado2000@gmail.com>
2023-12-04 10:20:11 +0200

    fix after-build script

--- Mma.Cli.UI/after-build.bat
+++ Mma.Cli.UI/after-build.bat
@@ -1,2 +1,2 @@
-﻿cd D:\Workspaces\dotnet-workspace\Mma.Cli\Mma.Cli.UI
-dotnet publish -c Release -o D:\Workspaces\dotnet-workspace\Mma.Cli\Mma.Cli.AppV4\UI\
+﻿cd .\Mma.Cli.UI
+dotnet publish -c Release -o ..\Mma.Cli.AppV4\UI\
